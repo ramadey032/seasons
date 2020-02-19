bf29b04d9ab7f9b47317bf323feac6d12fa8db08
ramaranjandey <36834875+ramaranjandey@users.noreply.github.com>
2020-02-19 20:45:50 +0530

    Second Commit

---
+++ src/Loader.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Loader = (props) => {
+    return (
+        <div className="ui active dimmer">
+            <div className="ui large text loader">{props.message}</div>
+        </div>
+    );
+}
+
+Loader.defaultProps = {
+    message: "Loading....."
+};
+
+export default Loader;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Loader from './Loader';
+
 
 class App extends React.Component {
     state = { lat: null, errorMessage: '' };
@@ -13,14 +15,21 @@ class App extends React.Component {
         );
     }
 
-    render() {
+    renderContent () {
         if (this.state.errorMessage && !this.state.lat) {
             return <div> error: {this.state.errorMessage} </div>;
         }
         if (this.state.lat && !this.state.errorMessage) {
             return <SeasonDisplay lat={this.state.lat} />
         }
-        return <div> Loading.........</div>;
+        return <div> <Loader message="Please Allow to read the location..."/> </div>;
+    }
+    render() {
+        return (
+            <div>
+                {this.renderContent()}
+            </div>
+        );
     }
 }
 
