3a333c0783d837744c9cf42b5178be2f5144f438
ramaranjandey <36834875+ramaranjandey@users.noreply.github.com>
2020-03-06 10:08:06 +0530

    updated : functional component replacement for class based component

--- src/index.js
+++ src/index.js
@@ -2,8 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import Loader from './Loader';
+import useLocation from './useLocation';
 
 
+const App = () => {
+
+    const [lat, errorMessage] = useLocation();
+
+    let content;
+    if (errorMessage) {
+        content = <div>Error: {errorMessage}</div>;
+    } else if (lat) {
+        content = <SeasonDisplay lat={lat} />;
+    } else {
+        content = <div> <Loader message="Please Allow to read the location..." /> </div>;
+    }
+
+    return <div className="border red">{content}</div>
+};
+
+
+// class based component approach
+/*
 class App extends React.Component {
     state = { lat: null, errorMessage: '' };
 
@@ -15,14 +35,14 @@ class App extends React.Component {
         );
     }
 
-    renderContent () {
+    renderContent() {
         if (this.state.errorMessage && !this.state.lat) {
             return <div> error: {this.state.errorMessage} </div>;
         }
         if (this.state.lat && !this.state.errorMessage) {
             return <SeasonDisplay lat={this.state.lat} />
         }
-        return <div> <Loader message="Please Allow to read the location..."/> </div>;
+        return <div> <Loader message="Please Allow to read the location..." /> </div>;
     }
     render() {
         return (
@@ -32,9 +52,7 @@ class App extends React.Component {
         );
     }
 }
-
-
-
+*/
 
 ReactDOM.render(
     <App />,
